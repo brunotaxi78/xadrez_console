67047df14d970068f137d481e3e7fd7ad7626ed7
Bruno Monteiro <brunotaxi78@outlook.pt>
2020-10-07 11:58:42 +0100

    Criacao do tabuleiro de xadrez

---
+++ xadrez_console/tabuleiro/Cor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console.tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Vermelha
+    }
+}
