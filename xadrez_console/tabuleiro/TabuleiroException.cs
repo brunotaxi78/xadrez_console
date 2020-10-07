51c56830ca8d56f67556c05487d2554b4995d33f
Bruno Monteiro <brunotaxi78@outlook.pt>
2020-10-07 13:02:32 +0100

    Adicionadas excepcoes personalizadas

---
+++ xadrez_console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console.tabuleiro
+{
+    class TabuleiroException : ApplicationException
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
